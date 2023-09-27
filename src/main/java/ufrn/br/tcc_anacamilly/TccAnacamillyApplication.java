5108917a7ed08fe36b67b3da96d8bf4f0cfe4f0f
Ana Camilly <camillyguedes2@gmail.com>
2023-09-27 14:46:01 -0300

    criação do projeto

---
+++ src/main/java/ufrn/br/tcc_anacamilly/TccAnacamillyApplication.java
@@ -0,0 +1,13 @@
+package ufrn.br.tcc_anacamilly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccAnacamillyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TccAnacamillyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ufrn/br/tcc_anacamilly/TccAnacamillyApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.br.tcc_anacamilly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccAnacamillyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
